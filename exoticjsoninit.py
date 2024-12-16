1aa307b5315432a7dce036df7e1650fc742ed379
mgarrod <matthew.garrod@me.com>
2024-12-16 10:45:23 -0500

    Update exoticjsoninit.py

--- exoticjsoninit.py
+++ exoticjsoninit.py
@@ -9,7 +9,7 @@ class ExoticJsonInit:
     def setUserInfoJsonData(self, planet):
         self.obs_data["user_info"]["Directory with FITS files"] = self.config.fits_files_dir + planet.replace(" ", "") + "/"
         self.obs_data["user_info"]["Directory to Save Plots"] = self.config.output_dir
-        self.obs_data["user_info"]["AAVSO Observer Code (N/A if none)"] = self.config.obs_code
+        self.obs_data["user_info"]["AAVSO Observer Code (N/A if none)"] = self.config.aavso_observer_code
 
         if self.config.flats is not None:
             self.observatory.obs_data["user_info"]["Directory of Flats"] = self.config.fits_files_dir + planet.replace(" ",
