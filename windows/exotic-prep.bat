1c084c5558b82f6b5a654a5752a579c9a5f16cfa
mgarrod <matthew.garrod@me.com>
2025-01-04 14:55:12 -0500

    Create exotic-prep.bat

---
+++ windows/exotic-prep.bat
@@ -0,0 +1,4 @@
+:: 1. Place this file in: C:\Users\{USERNAME}\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
+:: 2. Replace {USERNAME} with your Windows username
+:: 3. Restart Windows
+doskey exotic-prep=python "C:\Users\{USERNAME}\exotic-prep\exotic-prep.py"
